d6c54136ed6939d8f800b089c495f4ab8f0819fe
vidyashreeathani199 <vidyashreeathani.xworkz@gmail.com>
2021-10-08 16:45:35 +0530

    Method Overriding

---
+++ DenaBank.java
@@ -0,0 +1,10 @@
+class DenaBank extends Bank{
+
+
+
+public void provideLoans(double roi){
+System.out.println("Loan provided by Dena Bank");
+System.out.println(roi);
+
+}
+}
---
+++ SBIBank.java
@@ -0,0 +1,10 @@
+class SBIBank extends Bank{
+
+
+
+public void provideLoans(double roi){
+System.out.println("Loan provided by SBI Bank");
+System.out.println(roi);
+
+}
+}
